59efd98f4bc5e1610bf9e0780039600a9615519e
Jovaughn <Jovaughn@Jova-Laptop.home>
2014-07-27 05:24:23 -0400

    print all subsets

---
+++ src/main/java/com/jcomm/datastructures/RefObj.java
@@ -0,0 +1,16 @@
+package com.jcomm.datastructures;
+
+public class RefObj {
+	
+	private int val;
+
+	public int getVal() {
+		return val;
+	}
+
+	public void setVal(int val) {
+		this.val = val;
+	}
+	
+
+}
