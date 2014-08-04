d77660c8baac882f686b4db1ce63215ee1c30a26
SerialCoderer <jova626@gmail.com>
2014-08-04 13:39:12 -0400

    merge items

--- src/main/java/com/jcomm/datastructures/CollectionsHelper.java
+++ src/main/java/com/jcomm/datastructures/CollectionsHelper.java
@@ -1,6 +1,6 @@
-
 package com.jcomm.datastructures;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -10,134 +10,12 @@ import java.util.TreeMap;
 
 public class CollectionsHelper {
 
-	public static void main(String[] args) {
-
-		int a[] = { 1, 1, 1, 1, 2 };
-		System.out.println(CollectionsHelper.one2oneHundred(a));
-	}
-
-	static int[] mostFrequentSum(int n) {
-
-		if (n <= 0)
-			return new int[0];
-
-		int[] countFrequencyArray = new int[100000];
-		TreeMap<Integer, Integer> mostFrequentMap = new TreeMap<>();
-
-		for (int count = 0; count < countFrequencyArray.length; count++) {
-
-			int rolledVal = 0;
-
-			int numberOfRolls = n;
-			while (numberOfRolls > 0) {
-				rolledVal += rollDice();
-
-				numberOfRolls--;
-			}
-
-			int tempVal = 0;
-			if (mostFrequentMap.containsKey(rolledVal))
-				tempVal = mostFrequentMap.get(rolledVal);
-
-			mostFrequentMap.put(rolledVal, ++tempVal);
-		}
-
-		Map<Integer, Integer> mostFrequentNums = new TreeMap<>();
-		int mostFrequent = 0;
-		for (Map.Entry<Integer, Integer> entry : mostFrequentMap.entrySet()) {
-			int rolledDice = entry.getKey();
-			int rolledCount = entry.getValue();
-
-			System.out.println("rolled " + rolledDice + " " + rolledCount
-					+ " times.");
-
-			if (rolledCount > 0 && rolledCount > mostFrequent) {
-				mostFrequent = rolledCount;
-				mostFrequentNums = new HashMap<>();
-				mostFrequentNums.put(rolledDice, rolledCount);
-			} else if (rolledCount > 0 && rolledCount == mostFrequent) {
-				mostFrequentNums.put(rolledDice, rolledCount);
-			}
-		}
-
-		int arr[] = new int[mostFrequentNums.keySet().size()];
-		Object freq[] = mostFrequentNums.keySet().toArray();
-
-		for (int count = 0; count < arr.length; count++) {
-
-			arr[count] = (Integer) freq[count];
-		}
-
-		return arr;
-
-	}
-
-	public static void jo() {
-		int val = randomVal();
-		val += randomVal();
-		val += randomVal();
-		val += randomVal();
-		val += randomVal();
-
-		System.out.println(val);
-	}
-
-	//
-	static int one2oneHundred(int[] dice) {
-
-		int[] domain = new int[100];
-		int dice1 = dice[0];
-		int dice2 = dice[1];
-		int dice3 = dice[2];
-		int dice4 = dice[3];
-		int dice5 = dice[4];
-
-		for (int x = 0; x < domain.length; x++) {
-
-			domain[x] = x + 1;
-		}
-
-		int internalDice[] = new int[6];
-		for (int x = 0; x < internalDice.length; x++) {
-
-			domain[x] = x + 1;
-		}
-
-		int val = (dice2 + dice3 + dice4 + dice5) % 4;
-
-		return domain[val];
-
-	}
-
-	private static int randomVal() {
-
-		Random r = new Random();
-
-		int val = r.nextInt(21);
-
-		System.out.println(val);
-		return val;
-	}
-
-	static int rollDice() {
-
-		Random r = new Random();
-		int val = r.nextInt(7);
-		while (val == 0) {
-			val = r.nextInt(7);
-		}
-
-		return val;
-
-	}
-
 	public static void printArray(int a[]) {
 
 		for (int x : a) {
-			System.out.print(x + ",");
+			System.out.print(x+ ",");
 		}
-		
-		
+		System.out.println("");
 	}
 
 	public static <T> void printArray(T array[]) {
@@ -146,7 +24,7 @@ public class CollectionsHelper {
 			System.out.print(i + ",");
 		}
 
-		
+		System.out.println("");
 	}
 
 	public static Integer[] randomIntArray(int size) {
@@ -208,7 +86,7 @@ public class CollectionsHelper {
 		array[otherIndex] = array[index];
 		array[index] = tempVal;
 	}
-
+	
 	public static <T extends Comparable<T>> T[] heapSort(T array[]) {
 		int heapSize = 0; // heapSize is important to know in determining when
 							// to the recursive calls
@@ -266,46 +144,58 @@ public class CollectionsHelper {
 	private static <T> int getRightChildIndex(int index) {
 		return (2 * index) + 2;
 	}
-
-	public static void quickSort(int A[]) {
-
-		quickSort(A, 0, A.length);
+	
+	
+	public static void quickSort(int A[]){
+		
+		quickSort(A, 0, A.length-1);
 	}
 
-	private static void quickSort(int A[], int start, int end) {
-
-		if (end == start)
-			return;
+	private static void quickSort(int A[], int start, int end){
 		
-		int q = partition(A, start, end);
-		quickSort(A, start, q-1);
-		quickSort(A, q, end);
+		if(start==end)
+			return;
+			
+			int q = partition(A, start, end);
+			quickSort(A, start, q);
+			quickSort(A, q+1, end);
 	}
 
 	private static int partition(int A[], int start, int end) {
-		
-		if(start == end)
-			return start;
-		int i = start; // this will create a partition of items that are either
-		// greater or less than or equal to the pivot
-		int x = A[end - 1]; // this is the pivot
 
-		for (int j = start; j < end-1; j++) {
+		int i = start - 1;
+		int x = A[end];
 
-			if (A[j] <= x) {
+		for (int j = start; j < end; j++) {
+			if (A[j] < x) {
 				int temp = A[j];
-				A[j] = A[i];
-				A[i] = temp;
+				A[j] = A[i + 1];
+				A[i + 1] = temp;
 				i++;
-			}
+			}	
 		}
+		int temp = A[end];
+		A[end] = A[i+1];
+		A[i+1] = temp;
+		return i;
 
-		int q = (start + end) / 2;
-		int temp = A[i];
-		A[i] = A[end-1];
-		A[end-1] = temp;
-
-		return q;
 	}
+	public static void rotateArray(Integer []arr, int index, int rotateBy){
+
+		for(int x = 0; x<arr.length; x++){	
+			int newLocation = ((x + rotateBy)-1) % arr.length;
+			swapValues(arr, x, newLocation);
+		}
+	}
+	
+	public static <T> void printCollection (Collection<T> collection){
+		
+		for(T item : collection){
+			System.out.print(item+",");
+		}
+		System.out.println();
+	}
+	
+	
 
 }
