102043b6a6e6b886904fa98071ae806a1ee351a9
Jovaughn <Jovaughn@Jova-Laptop.home>
2014-08-26 07:55:25 -0400

    adding Backtrack implementation

---
+++ src/main/java/com/jcomm/algorithms/Backtrack.java
@@ -0,0 +1,36 @@
+package com.jcomm.algorithms;
+
+
+public abstract class Backtrack<T> {
+
+	protected boolean finished = false;
+	protected int maxCandidates  = 0;
+	
+	
+	public final void backtrack(int a[], int k, T input){
+		
+		int []possibleCandidates = new int[maxCandidates];
+		RefObj numCandidates = new RefObj();
+
+		if(isSolution(a, k, input))
+			processSolution(a,k,input);
+		 else{
+			k = k+1;
+			constructCandidates(a,k,input, possibleCandidates, numCandidates );
+			for(int i=0; i<numCandidates.getVal(); i++){		
+				a[k] = possibleCandidates[i];
+				make_move(a,k,input);
+				backtrack(a,k,input);
+				unmake_move(a,k,input);
+				if(finished) return;
+			}
+		}	
+	}
+	
+	public abstract boolean isSolution(int a[], int k, T input);
+	public abstract void processSolution(int a[], int k, T input);
+	public abstract void constructCandidates(int a[], int k, T input, int []possibleCandidates, RefObj ncandidates);
+	public abstract void make_move(int a[], int k, T input);
+	public abstract void unmake_move(int a[], int k, T input);
+
+}
---
+++ src/main/java/com/jcomm/algorithms/RefObj.java
@@ -0,0 +1,17 @@
+package com.jcomm.algorithms;
+
+
+public class RefObj {
+	
+	private int val;
+
+	public int getVal() {
+		return val;
+	}
+
+	public void setVal(int val) {
+		this.val = val;
+	}
+	
+
+}
