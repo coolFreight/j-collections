a9c477eb0ca718d7ed82a7c5eafb811c819045d6
jova <jova626@gmail.com>
2015-05-07 07:49:29 -0400

    BinaryTree conversion to linkList add selection sort and priorityqueue

--- src/main/java/com/jcomm/datastructures/PriorityQueue.java
+++ src/main/java/com/jcomm/datastructures/PriorityQueue.java
@@ -49,14 +49,14 @@ public class PriorityQueue<T extends Comparable> {
         return (int)Math.ceil((double)index/2)-1;
     }
 
-    private T getLeftChild(int index){
+    private int getLeftChild(int index){
 
-        return this.internalArray[index+1];
+        return (2*index)+1;
     }
 
-    private T getRightChild(int index){
+    private int getRightChild(int index){
 
-        return this.internalArray[index+2];
+        return (2*index)+2;
     }
 
     private void swapParentChild(int childPos, int parentPos){
@@ -76,6 +76,54 @@ public class PriorityQueue<T extends Comparable> {
             swapParentChild(pos, getParent(pos));
             bubbleUp(getParent(pos), elem);
         }
+    }
+
+    public T remove(){
+        if(size==0){
+            System.out.println("Q is empty");
+            return null;
+        }
+
+        T temp = this.internalArray[0];
+        this.internalArray[0] =   this.internalArray[size-1];
+        this.internalArray[size-1] = null;
+        size--;
+        bubbleDown(0);
+        return temp;
+    }
+
+    private void bubbleDown(int currentIndex){
+
+        int leftChildIndex = getLeftChild(currentIndex);
+        T left = this.internalArray[leftChildIndex];
+
+
+        int rightChildIndex = getRightChild(currentIndex);
+        T right  =  this.internalArray[rightChildIndex];
+
+        if(right== null && left==null)
+            return;
+
+        T parent = this.internalArray[currentIndex];
+        if((right!=null && left!=null)){
+
+            if(left.compareTo(right)<0 && left.compareTo(parent)<0){
+                swapParentChild(leftChildIndex, currentIndex);
+                bubbleDown(leftChildIndex);
+            } else if(left.compareTo(right)>0 && right.compareTo(parent)<0){
+                swapParentChild(rightChildIndex, currentIndex);
+                bubbleDown(rightChildIndex);
+            }
+            return;
+        }else if ((right == null && left != null) && parent.compareTo(left) > 0){
+            swapParentChild(leftChildIndex, currentIndex);
+            bubbleDown(leftChildIndex);
+        }else if((left == null && right !=null) && parent.compareTo(right)>0){
+            swapParentChild(rightChildIndex, currentIndex);
+            bubbleDown(rightChildIndex);
+        }
+
+
     }
 
 
@@ -100,6 +148,20 @@ public class PriorityQueue<T extends Comparable> {
             System.out.println(pq.peek());
              pq.insert(1);
             System.out.println(pq.peek());
+           Integer value = pq.remove();
+            System.out.println("Removed val :"+value+" top of q "+pq.peek());
+            value = pq.remove();
+            System.out.println("Removed val :"+value+" top of q "+pq.peek());
+            value = pq.remove();
+            System.out.println("Removed val :"+value+" top of q "+pq.peek());
+            value = pq.remove();
+            System.out.println("Removed val :"+value+" top of q "+pq.peek());
+            value = pq.remove();
+            System.out.println("Removed val :"+value+" top of q "+pq.peek());
+            value = pq.remove();
+            System.out.println("Removed val :"+value+" top of q "+pq.peek());
+            value = pq.remove();
+            System.out.println("Removed val :"+value+" top of q "+pq.peek());
 
         }catch(Exception e){
             System.out.println(e.getMessage());
